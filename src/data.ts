553a2113c69a481146e96fd47bda0b3ab1075926
dkirchhof <dkirchhof@gmail.com>
2019-05-13 18:51:02 +0200

    date prop in day interface for better readability

--- src/data.ts
+++ src/data.ts
@@ -11,9 +11,11 @@ export const data: IData = {
     "months": [
         [
             {
+                "date": "2019-01-01",
                 "should": 0
             },
             {
+                "date": "2019-01-01",
                 "should": 480,
                 "start": 480,
                 "end": 960,
@@ -21,99 +23,73 @@ export const data: IData = {
                 "sick": 30, 
             },
             {
+                "date": "2019-01-01",
                 "should": 480,
                 "holiday": 480
             },
             {
+                "date": "2019-01-01",
                 "should": 480,
                 "start": 480,
                 "end": 1060,
                 "break": 30
             },
             {
+                "date": "2019-01-01",
                 "should": 0,
             },
             {
+                "date": "2019-01-02",
                 "should": 0
             },
             {
+                "date": "2019-01-03",
                 "should": 480
             },
             {
+                "date": "2019-01-04",
                 "should": 480
             },
             {
+                "date": "2019-01-05",
                 "should": 480
             },
             {
+                "date": "2019-01-06",
                 "should": 480
             },
             {
+                "date": "2019-01-07",
                 "should": 480
             },
             {
+                "date": "2019-01-08",
                 "should": 0
             },
             {
+                "date": "2019-01-09",
                 "should": 0
             },
             {
+                "date": "2019-01-10",
                 "should": 480
             },
             {
+                "date": "2019-01-11",
                 "should": 480,
             },
             {
+                "date": "2019-01-12",
                 "should": 480
             },
             {
+                "date": "2019-01-13",
                 "should": 480
             },
-            {
-                "should": 480
-            },
-            {
-                "should": 0
-            },
-            {
-                "should": 0
-            },
-            {
-                "should": 480
-            },
-            {
-                "should": 480
-            },
-            {
-                "should": 480
-            },
-            {
-                "should": 480
-            },
-            {
-                "should": 480
-            },
-            {
-                "should": 0
-            },
-            {
-                "should": 0
-            },
-            {
-                "should": 480
-            },
-            {
-                "should": 480
-            },
-            {
-                "should": 480
-            },
-            {
-                "should": 480
-            }
         ],
         [
             {
+                "date": "2019-02-01",
                 "should": 480
             }
         ]
--- src/models/day.ts
+++ src/models/day.ts
@@ -1,4 +1,5 @@
 export interface IDay {
+    date: string;
     should: number;
 
     start?: number;
